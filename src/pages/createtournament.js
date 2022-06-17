54e6c35e045cfeebd70deee014eb61ded7be2713
CommonSenze <41241538+CommonSenze@users.noreply.github.com>
2022-06-16 23:39:03 -0500

    Update createtournament.js

--- src/pages/createtournament.js
+++ src/pages/createtournament.js
@@ -6,6 +6,7 @@ import Navbar from '../components/navbar'
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { DateTime } from 'luxon'
 import Footer from '../components/footer'
+import { create } from '@mui/material/styles/createTransitions'
 
 async function createTournament(data) {
     fetch("localhost:8080/api/event/create", {
@@ -23,8 +24,6 @@ function formatBytes(bytes, decimals = 2) {
 
     const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-
-
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + i;
 }
 
@@ -32,6 +31,7 @@ function CreateTournament() {
     const [date, setDate] = useState(new Date());
     const [amount, setAmount] = useState(0);
     const [invalidImg, setInvalidImg] = useState(false);
+    const [invalidForm, setInvalidForm] = useState("");
 
     const changeAmount = (event) => {
         let number = event.target.value;
@@ -63,9 +63,9 @@ function CreateTournament() {
             img.onload = function () {
                 const imgWidth = img.naturalWidth;
                 const imgHeight = img.naturalHeight;
-    
+
                 res({
-                    width: imgWidth, 
+                    width: imgWidth,
                     height: imgHeight
                 });
             };
@@ -74,6 +74,14 @@ function CreateTournament() {
             img.src = res;
         })
         return promise;
+    }
+
+    const changeInvalidImg = (value) => {
+        setInvalidImg(value);
+    }
+
+    function isFormValid() {
+        return (invalidForm === "");
     }
 
     return (<>
@@ -91,17 +99,41 @@ function CreateTournament() {
                 <Divider />
                 <Box component="form" id="eventForm" padding="15px" onSubmit={event => {
                     event.preventDefault();
+                    setInvalidForm("");
                     var form = new FormData(document.forms.eventForm);
-                    var data = {
-                        name: form.get("event-name"),
-                        description: form.get("event-description"),
-                        date: form.get("event-date"),
-                        img: form.get("event-img"),
-                        name: form.get("event-name"),
-                    }
-                    console.log(form.get("event-img"))
+                    getBase64(form.get("event-img")).then(res => {
+                        var data = {
+                            name: form.get("event-name"),
+                            description: form.get("event-description"),
+                            winnings: form.get("event-winnings"),
+                            date: form.get("event-date"),
+                            image: res,
+                        }
+                        let incorrect = "";
+                        for (const key in data) {
+                            let obj = data[key];
+                            if (!obj
+                                || Object.keys(obj).length === 0
+                                || Object.getPrototypeOf(obj) === Object.prototype) {
+                                incorrect += key + ", ";
+                            }
+                        }
+                        if (invalidImg) {
+                            incorrect += "image size";
+                        } else {
+                            incorrect = incorrect.substring(0, incorrect.length - 2);
+                        }
+
+                        if (incorrect !== "") {
+                            setInvalidForm("Form is incorrect. Current values are not valid: " + incorrect + ".")
+                            return;
+                        }
+
+                        createTournament(data);
+                    })
                 }}>
                     <Box marginBottom="10px">Please ensure you type all the infromation correctly.</Box>
+                    <Box marginBottom="10px" color="red" display={isFormValid() ? "none" : "block"}>{invalidForm}</Box>
                     <Box marginBottom="10px">
                         <FormControl fullWidth variant="outlined">
                             <OutlinedInput name="event-name" inputProps={{ style: { fontSize: "15px", fontWeight: "bold" } }} id="component-simple" placeholder="Name" />
@@ -131,20 +163,16 @@ function CreateTournament() {
                     </Box>
                     <Box marginBottom="10px">
                         <Box display="block" marginBottom="5px">Upload Image Thumbnail</Box>
-                        <Box display="block" marginBottom="5px">Image Must be Under </Box>
+                        <Box marginBottom="5px" color="red" display={invalidImg ? "block" : "none"}>Image Must be Under 1MB</Box>
                         <input type="file" id="myFile" onChange={(event) => {
                             let file = event.target.files[0];
                             let size = formatBytes(file.size).split(" ");
-                            if (size[0] > 1&&size[1] >= 2){
-                                console.log("MAX")
+                            if (size[0] > 1 && size[1] >= 2) {
+                                changeInvalidImg(true);
                                 event.preventDefault()
-                                return;
+                            } else if (invalidImg) {
+                                changeInvalidImg(false);
                             }
-                            getImageSize(file).then(res => {
-                                if (res.width) {
-
-                                }
-                            })
                         }} name="event-img" />
                     </Box>
 
